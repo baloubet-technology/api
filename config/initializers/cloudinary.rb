faaea4833c3b9bf8ab6f058505745c9a891d2806
hugo mori <mori.hugo@outlook.com>
2020-11-15 20:50:45 +0100

    fix: change cloud_name cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'products'
+  config.cloud_name = 'baloubet'
   config.api_key = '287572326864126'
   config.api_secret = 'RkOI-Dv8Y2IWK8x8LeGfDY1qMqM'
 end
