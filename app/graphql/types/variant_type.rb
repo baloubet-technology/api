a06357e41d85546fb09aa38b7071c742320b2a62
hugo mori <mori.hugo@outlook.com>
2020-11-15 17:35:58 +0100

    fix: change query null true

--- app/graphql/types/variant_type.rb
+++ app/graphql/types/variant_type.rb
@@ -1,20 +1,20 @@
 module Types
   class VariantType < Types::BaseObject
     field :id, ID, null: false
-    field :sku, String, null: false
-    field :quantity, Integer, null: false
-    field :price, Float, null: false
-    field :price_cents, Integer, null: false
-    field :size, String, null: false
-    field :color, String, null: false
-    field :picture_url, String, null: false
-    field :status, String, null: false
+    field :sku, String, null: true
+    field :quantity, Integer, null: true
+    field :price, Float, null: true
+    field :price_cents, Integer, null: true
+    field :size, String, null: true
+    field :color, String, null: true
+    field :picture_url, String, null: true
+    field :status, String, null: true
 
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
 
-    field :organization, OrganizationType, null: false
-    field :product, ProductType, null: false
-    field :order, [Types::OrderType], null: false
+    field :organization, OrganizationType, null: true
+    field :product, ProductType, null: true
+    field :order, [Types::OrderType], null: true
   end
 end
